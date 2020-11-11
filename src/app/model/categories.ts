9a22bbbea97784f362749231d1a011af0462d280
Marius <marius.stanescu@re.ro>
2020-11-11 11:34:07 +0200

    Add mock products and categories

---
+++ src/app/model/categories.ts
@@ -0,0 +1,8 @@
+import { Category } from './category';
+
+export const Categories: Category[] = [
+    { id: 1, name: 'Men clothing' },
+    { id: 2, name: 'Women clothing' },
+    { id: 3, name: 'Jewelery' },
+    { id: 4, name: 'Electronics' },
+];

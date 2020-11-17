77ebfad407cc52503813c4ce66dbd0abcf8e12c0
Marius <marius.stanescu@arnia.ro>
2020-11-17 14:27:40 +0200

    Add DumbComponent abstract class

--- src/app/cart/cart-details.component.ts
+++ src/app/cart/cart-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DumbComponent } from '../_helpers/dumb-component';
 import { CartVm } from './cart-vm';
 
 @Component({
@@ -6,15 +7,13 @@ import { CartVm } from './cart-vm';
   templateUrl: './cart-details.component.html',
   styleUrls: ['./cart-details.component.css']
 })
-export class CartDetailsComponent implements OnInit {
+export class CartDetailsComponent extends DumbComponent {
 
   @Input()
   public cart: CartVm;
 
   constructor() {
-  }
-
-  ngOnInit(): void {
+    super();
   }
 
 }

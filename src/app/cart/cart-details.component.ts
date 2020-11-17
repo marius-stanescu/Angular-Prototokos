f50ef6037934761ac9403beee930e3b96d3b8ac3
Marius <marius.stanescu@arnia.ro>
2020-11-17 16:09:06 +0200

    Use on push change detection strategy on dumb component

--- src/app/cart/cart-details.component.ts
+++ src/app/cart/cart-details.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { DumbComponent } from '../_helpers/dumb-component';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { DumbComponent } from '../dumb-component';
 import { CartVm } from './cart-vm';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrls: ['./cart-details.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartDetailsComponent extends DumbComponent {
 

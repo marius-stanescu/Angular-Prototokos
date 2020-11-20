d8f178f23a1514411ff4d2213b948c1d58fad13c
Marius <marius.stanescu@arnia.ro>
2020-11-20 12:23:27 +0200

    Use observable cart and async pipe

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Cart, CartItem } from '../model/cart';
 import { SmartComponent } from '../smart-component';
 import { RemoveFromCart } from '../store/cart.actions';
@@ -11,11 +12,11 @@ import { RemoveFromCart } from '../store/cart.actions';
 })
 export class CartComponent extends SmartComponent {
 
-  public cart: Cart;
+  public cart$: Observable<Cart>;
 
   constructor(private store: Store<{ cart: Cart }>) {
     super();
-    store.pipe(select('cart')).subscribe(data => this.cart = data);
+    this.cart$ = store.pipe(select('cart'));
   }
 
   ngOnInit(): void {

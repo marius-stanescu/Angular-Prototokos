1494ea1f232cc53608c3efd76a6a93b6d0c8e461
Marius <marius.stanescu@arnia.ro>
2020-11-23 16:49:38 +0200

    Add product extras model and data

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,4 +1,7 @@
 export class Product {
+
+    public extras: ProductExtraCategory[] = [];
+
     constructor(
         public id: number,
         public title: string,
@@ -6,4 +9,18 @@ export class Product {
         public categoryId: string,
         public description?: string,
         public image?: string) { }
+}
+
+export class ProductExtraCategory {
+    constructor(
+        public name: string,
+        public isExclusive: boolean,
+        public isMandatory: boolean,
+        public extras: ProductExtra[]) { }
+}
+
+export class ProductExtra {
+    constructor(
+        public name: string,
+        public price: number) { }
 }
--- src/app/store/product.effects.ts
+++ src/app/store/product.effects.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { EMPTY } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
+import { Product, ProductExtra, ProductExtraCategory } from '../model/product';
 import { ProductService } from '../_services/product.service';
 import { LoadProducts, LoadProductsSuccess, ProductActionTypes } from './product.actions';
 
@@ -17,7 +18,35 @@ export class ProductEffects {
         ofType(ProductActionTypes.LoadProducts),
         mergeMap((action: LoadProducts) =>
             this.productService.getProducts(action.categoryId).pipe(
-                map(products => {
+                map(data => {
+                    let products = data.map(p => {
+                        let product = new Product(p.id, p.title, p.price,
+                            p.category, p.description, p.image);
+                        if (product.categoryId.includes('clothing')) {
+                            product.extras = [
+                                new ProductExtraCategory('Size', true, true, [
+                                    new ProductExtra('Small', 0),
+                                    new ProductExtra('Medium', product.price * 5 / 100),
+                                    new ProductExtra('Large', product.price * 10 / 100),
+                                ]),
+                                new ProductExtraCategory('Color', true, true, [
+                                    new ProductExtra('Red', 0),
+                                    new ProductExtra('Green', 0),
+                                    new ProductExtra('Blue', 0),
+                                    new ProductExtra('Rainbow', 10),
+                                ]),
+                            ];
+                        } else if (action.categoryId.includes('jewelery')) {
+                            product.extras = [
+                                new ProductExtraCategory('Gift wrapping', false, false, [
+                                    new ProductExtra('Wrapping paper', 2),
+                                    new ProductExtra('Gift box', 10),
+                                    new ProductExtra('Gift bag', 5),
+                                ]),
+                            ];
+                        }
+                        return product;
+                    });
                     return new LoadProductsSuccess(products);
                 }),
                 catchError(() => EMPTY)
